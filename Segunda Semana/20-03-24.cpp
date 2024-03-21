1109e69795d81f4be7d6f60b8ba67be2e1c7fadb
ALUMNOP-104 <ALUMNOP-104@UCSP.AULAB>
2024-03-21 08:03:47 -0500

    21-03-24

---
+++ Segunda Semana/20-03-24.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int length{0};
+    int Area{0};
+    int Volum{0};
+
+    //Area = 6 * (length * length);
+    //Volum = length * length * length;
+
+    length = 0;
+    //cout << "Lenght " << length << endl;
+    Area = 6 * (length * length);
+    Volum = length * length * length;
+    //cout << "Area is " << Area << " And Volume is " << Volum << endl;
+    cout << length << "\t\t\t\t" << Area << "\t\t\t\t" << Volum << "\t\t\t\t" << endl;
+
+    length = 1;
+    //cout << "Lenght " << length << endl;
+    Area = 6 * (length * length);
+    Volum = length * length * length;
+    //cout << "Area is " << Area << " And Volume is " << Volum << endl;
+    cout << length << "\t\t\t\t" << Area << "\t\t\t\t" << Volum << "\t\t\t\t" << endl;
+
+    length = 2;
+    //cout << "Lenght " << length << endl;
+    Area = 6 * (length * length);
+    Volum = length * length * length;
+    //cout << "Area is " << Area << " And Volume is " << Volum << endl;
+    cout << length << "\t\t\t\t" << Area << "\t\t\t\t" << Volum << "\t\t\t\t" << endl;
+
+    length = 3;
+    //cout << "Lenght " << length << endl;
+    Area = 6 * (length * length);
+    Volum = length * length * length;
+    //cout << "Area is " << Area << " And Volume is " << Volum << endl;
+    cout << length << "\t\t\t\t" << Area << "\t\t\t\t" << Volum << "\t\t\t\t" << endl;
+
+    length = 4;
+    //cout << "Lenght " << length << endl;
+    Area = 6 * (length * length);
+    Volum = length * length * length;
+    //cout << "Area is " << Area << " And Volume is " << Volum << endl;
+    cout << length << "\t\t\t\t" << Area << "\t\t\t\t" << Volum << "\t\t\t\t" << endl;
+
+    return 0;
+}
